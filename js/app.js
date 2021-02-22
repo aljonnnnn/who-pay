88fe8e64113078f91edcab9769642dfd515fe4fb
aljonnnnn <delumenaljon@gmail.com>
2021-02-22 17:36:54 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const addNameBtn = document.querySelector('.add-btn');
+const whoPayBtn = document.querySelector('.who-pay-btn');
+const listName = [];
+
+addNameBtn.addEventListener('click', function() {
+    const name = document.querySelector('.input-name').value;
+
+    if (name !== '') {
+        listName.push(name);
+    } else {
+        alert('type name in the input')
+    }
+
+    document.querySelector('.input-name').value = '';
+})
+
+whoPayBtn.addEventListener('click', function() {
+    const randomNumber = Math.floor(Math.random() * listName.length);
+    
+    const winner =  document.querySelector('.unlucky');
+    winner.textContent = '👌 '+ listName[randomNumber] +' 👏';
+    console.log(winner.textContent);
+})
+
+
+const enterInput = document.querySelector('.input-name');
+enterInput.addEventListener('keypress', function(event) {
+    const name = document.querySelector('.input-name').value;
+
+    if (event.keyCode === 13) {
+        if (name !== '') {
+            listName.push(name);
+        }
+        
+        document.querySelector('.input-name').value = '';
+    }
+})
